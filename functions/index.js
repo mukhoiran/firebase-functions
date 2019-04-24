0304a2275805a3bb7cff65c083575dca75c6a7c0
mukhoiran <mkhoir.anam@gmail.com>
2019-04-25 00:39:58 +0700

    routes nodejs

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
-const functions = require('firebase-functions');
+var functions = require('firebase-functions');
+var express   = require('express');
+var app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// routes
+app.get('/mukhoiran', function(req, res){
+  res.send("hi.. we can play backend in firebase");
+})
+
+exports.app = functions.https.onRequest(app);
