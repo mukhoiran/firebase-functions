470b2c3fcda662e08ba4143ab72d1433a2cbe9ec
mukhoiran <mkhoir.anam@gmail.com>
2019-04-25 00:58:56 +0700

    template engine handlebars

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,25 @@
 var functions = require('firebase-functions');
 var express   = require('express');
+var cons      = require('consolidate');
+
 var app = express();
+app.engine('hbs', cons.handlebars);
+app.set('view engine','hbs');
+app.set('views','./views');
 
 // routes
 app.get('/mukhoiran', function(req, res){
   res.send("hi.. we can play backend in firebase");
+})
+
+var forumData = [
+    {"title": "How to start?"},
+    {"title": "Really confuse, need advice.."}
+];
+
+//routes
+app.get('/forum', function(req, res){
+  res.render('forum', {forums:forumData})
 })
 
 exports.app = functions.https.onRequest(app);
